7d87dbfcdee98bdf70bb76ac29816bc1ce6c2f1e
taejunha <tha7@uwo.ca>
2023-11-26 15:38:24 -0500

    organizational changes

---
+++ src/components/Footer.js
@@ -0,0 +1,8 @@
+// credits
+export function Footer() {
+    return (
+        <footer className="footer" align="center">
+            <p>Created by Taejun Ha</p>
+        </footer>
+    )
+}
---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+export function Header() {
+    return (
+        <header>
+            <h1>Spotify Stats</h1>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="/home">Home</a>
+                    </li>
+                    <li>
+                        <a href="/toptracks">Top Tracks</a>
+                    </li>
+                    <li>
+                        <a href="/topartists">Top Artists</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
