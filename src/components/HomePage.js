50d04ec625081eb8b556064d97f39d16d8d88702
Taejun Ha <Taejun Ha>
2024-06-27 16:33:39 -0400

    small login change

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -3,16 +3,12 @@ import React, { useState, useEFfect } from 'react';
 import "../style/homepage.css"
 
 export function HomePage() {
-    const handleLogin = () => {
-        window.location.href = `http://localhost:3000/login`;
-    };
-
     return (
         <>
         <p className="info" align="center">
             Discover your most played tracks and favourite artists in the past month, six months, or twelve months!
         </p>
-        <button className="login" role="button" onClick={handleLogin} >Login with Spotify</button>
+        <button className="login" role="button" >Login with Spotify</button>
 
         </>
     )
